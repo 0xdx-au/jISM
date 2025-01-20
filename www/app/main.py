29acaf0c19074c13c95e4146052a107c3661977a
0xdx-au <130902630+0xdx-au@users.noreply.github.com>
2025-01-20 14:55:52 +1100

    s

--- www/app/main.py
+++ www/app/main.py
@@ -1,6 +1,9 @@
-from app import create_app
+from flask import Flask, render_template
+app = Flask(__name__)
 
-app = create_app()
+@app.route('/')
+def index():
+    return "jISM is running!"
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
